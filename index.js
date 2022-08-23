bb88c1fd4ab33201daf57398033b2d19a2a95d18
Aleksandra <alexandra.bakhcheva@gmail.com>
2022-08-23 23:40:16 +0300

    files added

---
+++ index.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    let div = document.createElement("div");
+    div.innerHTML = `<h1>Мой github:</h1>
+    <div class="info"></div>`;
+    document.body.append(div);
+    fetch("https://api.github.com/users/AleksandraBakhcheva")
+    .then(response => response.json())
+    .then(user => {
+        console.log(user);
+        document.querySelector(".info").innerHTML = "Логин: " + user.login + "</br>" + "Количество репозиториев: " + user.public_repos + "</br>" + "Дата создания: " + user.created_at;
+    
+    })
+    .catch(error => console.log(error));
+    let projects = document.createElement("div");
+    projects.innerHTML = `<h1>Мои проекты:</h1>
+    <div class="projects"></div>`;
+    document.body.append(projects);
+    fetch("https://api.github.com/users/AleksandraBakhcheva/repos")
+    .then(response => response.json())
+    .then(projects => {
+        console.log(projects);
+        for (let i = 0; i < projects.length; i++) {
+            document.querySelector(".projects").innerHTML += projects[i].name + "</br>";
+
+        }
+    })
+    .catch(error => console.log(error));
+});
